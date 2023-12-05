f0cfb2fd18613424fc165037246146de39e8b739
SelinaZheng <selinazhenglx@gmail.com>
2023-12-05 14:22:54 -0500

    .

---
+++ main.js
@@ -0,0 +1,39 @@
+let playSound1 = function () {
+    document.getElementById("audio1").play();
+};
+let playSound2 = function () {
+    document.getElementById("audio2").play();
+};
+let playSound3 = function () {
+    document.getElementById("audio3").play();
+};
+let playSound4 = function () {
+    document.getElementById("audio4").play();
+};
+let playSound5 = function () {
+    document.getElementById("audio5").play();
+};
+let playSound6 = function () {
+    document.getElementById("audio6").play();
+};
+let playSound7 = function () {
+    document.getElementById("audio7").play();
+};
+let playSound8 = function () {
+    document.getElementById("audio8").play();
+};
+let playSound9 = function () {
+    document.getElementById("audio9").play();
+};
+let playSound10 = function () {
+    document.getElementById("audio10").play();
+};
+let playSound11 = function () {
+    document.getElementById("audio11").play();
+};
+let playSound12 = function () {
+    document.getElementById("audio12").play();
+};
+let playSound13 = function () {
+    document.getElementById("audio13").play();
+};
